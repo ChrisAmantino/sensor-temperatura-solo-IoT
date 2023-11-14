5e66ef62b1259590a1fb42255cbed1b01278ca88
ChrisAmantino <chris_amantino97@hotmail.com>
2023-11-14 18:14:11 -0300

    feat(v1): Atualização do código com anotações e inserção de prints para melhor visualização

--- codigo_sensor_temperatura.ino
+++ codigo_sensor_temperatura.ino
@@ -2,11 +2,16 @@
 #include <DallasTemperature.h>
 #include "EspMQTTClient.h"
 
+// Inicializa o barramento OneWire no pino digital 4
 OneWire barramento(4);
+
+// Cria um objeto DallasTemperature associado ao barramento OneWire
 DallasTemperature sensor(&barramento);
 
+// Define o pino digital 5 como BUZZER
 #define BUZZER 5
 
+// Configura o cliente MQTT com as informações de conexão
 EspMQTTClient client(
   "Wokwi-GUEST",         // SSID WiFi
   "",                    // Password WiFi
@@ -17,39 +22,64 @@ EspMQTTClient client(
 
 void setup()
 {
+   // Inicializa a comunicação serial a 115200 bps
    Serial.begin(115200);
+
+   // Inicializa o sensor DS18B20
    sensor.begin();
+
+   // Configura o pino BUZZER como saída
    pinMode(BUZZER, OUTPUT);
 
+   // Ativa mensagens de depuração no cliente MQTT
    client.enableDebuggingMessages();
+
+   // Ativa a atualização via HTTP no cliente MQTT
    client.enableHTTPWebUpdater();
+
+   // Ativa o OTA (Over-The-Air update) no cliente MQTT
    client.enableOTA();
+
+   // Configura uma mensagem "Last Will" a ser publicada se a conexão MQTT for perdida
    client.enableLastWillMessage("TestClient/lastwill", "Vou ficar offline");
 }
 
+// Função para ler a temperatura e enviar dados
 void lerEnviarDados()
 {
+  // Solicita a leitura da temperatura ao sensor
+  Serial.println("Capturando Temperatura");
   sensor.requestTemperatures();
+
+  // Lê a temperatura do sensor
   float temperatura = sensor.getTempCByIndex(0);
+
+  // Imprime a temperatura no monitor serial
   Serial.println(temperatura);
 
-  if (temperatura == -127) {
+  // Verifica se a temperatura é -127°C (valor de erro)
+  if (temperatura == 25) {
+    // Aciona o BUZZER por 1 segundo
+    Serial.println("Temperatura limite atingida, emitindo sinal sonoro!");
     digitalWrite(BUZZER, HIGH);
     delay(1000);
     digitalWrite(BUZZER, LOW);
-    // Envie um alerta MQTT quando a temperatura atingir -127°C no mesmo tópico
+
+    // Publica uma mensagem MQTT informando a condição
     client.publish("topicowokwi/Temp", "Temperatura atingiu -127°C");
   }
 }
 
+// Função chamada quando a conexão MQTT é estabelecida
 void onConnectionEstablished() {
   Serial.println("Conexão MQTT estabelecida com sucesso!");
-  // Insira aqui o que deseja fazer quando a conexão é estabelecida.
 }
 
 void loop()
 {
+  // Chama a função para ler e enviar dados de temperatura
   lerEnviarDados();
-  // Execute o loop MQTT
+
+  // Executa o loop MQTT para processar mensagens e manter a conexão
   client.loop();
 }
